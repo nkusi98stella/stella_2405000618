e4b68a3cade18a94ec29bd96cdf06b5a8d4664a0
nkusi98stella <nkusistella19@gmail.com>
2025-04-13 10:12:22 +0200

    Add files via upload
    submit my php work

---
+++ sites.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>
+
+    </title>
+</head>
+<body>
+<?php
+    $characterName = "David";
+    $characterAge = 35;
+    echo "<h1>Our first PHP </h1>";
+    echo "<hr>";
+    echo "<p>This second line paragraphy </p>";
+    echo "there once host to our webpage<br> ";
+    echo "it's now necessary<br>";
+?>
+</body>
+</html>
+
